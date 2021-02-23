2daf3e2251d847362c56b5ae2d24cd33ca940115
Jerry <mchen81@dons.usfca.edu>
2021-02-22 23:59:51 -0800

    add Offer Service

---
+++ OfferService/src/main/java/io/hashimati/offerservice/constants/Roles.java
@@ -0,0 +1,7 @@
+package io.hashimati.offerservice.constants;
+
+public class Roles {    
+    public static final String SERVICE_PROVIDER = "service_provider"; 
+    public static final String USER = "user"; 
+    
+}
