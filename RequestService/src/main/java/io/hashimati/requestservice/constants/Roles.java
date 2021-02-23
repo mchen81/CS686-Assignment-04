1fb9c2d98ee863fde9638ea7e3a41f2efebbe54d
Jerry <mchen81@dons.usfca.edu>
2021-02-22 23:58:26 -0800

    add Request Service

---
+++ RequestService/src/main/java/io/hashimati/requestservice/constants/Roles.java
@@ -0,0 +1,9 @@
+package io.hashimati.requestservice.constants;
+
+public class Roles {
+
+    
+    public static final String SERVICE_PROVIDER = "service_provider"; 
+    public static final String USER = "user"; 
+    
+}
---
+++ RequestService/src/main/java/io/hashimati/requestservice/services/Command.java
@@ -0,0 +1,7 @@
+package io.hashimati.requestservice.services;
+
+public class Command {
+	public static final String REJECT = "reject";
+	public static final String ACCEPT = "accept";
+
+}
